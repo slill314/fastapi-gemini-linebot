9603db9ce458f37ea406bf41f05eeef6bf466e0a
Zmado <126371549+slill314@users.noreply.github.com>
2024-09-19 15:17:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ async def callback(request:Request):
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    user_id = event.source.user_id  # 取得用戶的 user_id
+    print(f"User ID: {user_id}")  # 打印用戶 ID（可以用來 debug）
     
     allowed_user_ids = ["USER1234", "USER5678"]  # 允許的 user_id 清單
 
@@ -35,7 +38,8 @@ def handle_message(event):
         # 如果用戶不在允許清單中，回應一個提示消息
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="你無權使用這個功能。")
+            #TextSendMessage(text="你無權使用這個功能。")
+            TextSendMessage(text=f"你無權使用這個功能。你的 User ID 是: {user_id}")
         )
         return
  
