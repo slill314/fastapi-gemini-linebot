1c004837b49751867cbc71a95319dd63496ba17e
Zmado <126371549+slill314@users.noreply.github.com>
2024-10-08 12:23:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,8 +62,8 @@ def handle_message(event):
 
     if event.message.text == "新聞":
         #reply_msg = get_news.scrape_news()
-        #news_list = scrape_news()
-        news_list = []
+        news_list = scrape_news()
+        #news_list = []
         if news_list:  # 確認 news_list 不為空
             reply_msg = "\n\n".join(news_list)  # 將消息列表合併為一個字串
         else:
