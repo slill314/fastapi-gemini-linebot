603908ddeb78119682a120564ad71ef2d40f72ab
Zmado <126371549+slill314@users.noreply.github.com>
2024-10-08 15:13:30 +0800

    Update get_news.py

--- api/get_news.py
+++ api/get_news.py
@@ -14,20 +14,11 @@ def fetch_url_with_retry(url, headers):
 
 # 抓取新闻标题和链接的函数，限制只抓取前5则
 def scrape_news():
-    # 获取当前日期和时间
-    #current_time = datetime.now()
-
     #變換ua
     ua = UserAgent()
     user_agent = ua.random
-
     headers = {'user-agent': user_agent}
-    #print(headers)
-
-    # 格式化并打印当前时间
-    #formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    #print("執行時間：", formatted_time)
-
+    
     try:
         response = fetch_url_with_retry(url, headers=headers)
         #print("第一次就成功", formatted_time)
@@ -38,15 +29,8 @@ def scrape_news():
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        news_items = soup.find_all('div', class_='part_list_2')
-        if news_items:
-            for i, item in enumerate(news_items[:5]):  # 获取前五则新闻
-                title = item.find('h3').text
-                relative_link = item.find('a')['href']
-                full_link = urljoin(url_fornews, relative_link)  # 将相对链接转换为完整链接
-                message = f"隨選新聞: {title}\n網址: {full_link}"          
-                
-    return title
+        news_items = soup.find_all('div', class_='part_list_2')      
+    return news_items
     
     
 
