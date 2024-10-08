169d0fbda24c06f994d16c25cd89f6e83326ef3b
Zmado <126371549+slill314@users.noreply.github.com>
2024-10-08 16:40:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,45 +3,45 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from api.gemini import Gemini
 #from api.test import fetch_and_save_news_as_json
-#from api.get_news import scrape_news
+from api.get_news import scrape_news
 
-import requests
-import time
-from datetime import datetime
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from fake_useragent import UserAgent
+#import requests
+#import time
+#from datetime import datetime
+#from bs4 import BeautifulSoup
+#from urllib.parse import urljoin
+#from fake_useragent import UserAgent
 
 # 要抓取的新闻网页
-url = "https://www.ettoday.net/news/news-list.htm"
-url_fornews = "https://www.ettoday.net/news/"
+#url = "https://www.ettoday.net/news/news-list.htm"
+#url_fornews = "https://www.ettoday.net/news/"
 
-def fetch_url_with_retry(url, headers):
-    return requests.get(url, headers=headers)
+#def fetch_url_with_retry(url, headers):
+#    return requests.get(url, headers=headers)
 # 抓取新闻标题和链接的函数，限制只抓取前5则
-def scrape_news():
-    #變換ua
-    ua = UserAgent()
-    user_agent = ua.random
-    headers = {'user-agent': user_agent}
-    
-    try:
-        response = fetch_url_with_retry(url, headers=headers)
-        #print("第一次就成功", formatted_time)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        response.raise_for_status()
-        #print(f"Error fetching URL: {e}")
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        news_items = soup.find_all('div', class_='part_list_2')
-        for i, item in enumerate(news_items[:5]):
-            title = item.find('h3').text
-            relative_link = item.find('a')['href']
-            full_link = urljoin(url_fornews, relative_link)  # 将相对链接转换为完整链接
-            message = f"隨選新聞: {title}\n網址: {full_link}"
-    return message
+#def scrape_news():
+#    #變換ua
+#    ua = UserAgent()
+#    user_agent = ua.random
+#    headers = {'user-agent': user_agent}
+#    
+#    try:
+#        response = fetch_url_with_retry(url, headers=headers)
+#        #print("第一次就成功", formatted_time)
+#        response.raise_for_status()
+#    except requests.exceptions.RequestException as e:
+#        response.raise_for_status()
+#        #print(f"Error fetching URL: {e}")
+#
+#    if response.status_code == 200:
+#        soup = BeautifulSoup(response.text, 'html.parser')
+#        news_items = soup.find_all('div', class_='part_list_2')
+#        for i, item in enumerate(news_items[:5]):
+#            title = item.find('h3').text
+#            relative_link = item.find('a')['href']
+#            full_link = urljoin(url_fornews, relative_link)  # 将相对链接转换为完整链接
+#            message = f"隨選新聞: {title}\n網址: {full_link}"
+#    return message
 
 import os
 
