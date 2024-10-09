8e01310e6ea5853b302e9453b8649420b4cda4ec
Zmado <126371549+slill314@users.noreply.github.com>
2024-10-09 12:08:11 +0800

    Update get_news.py

--- api/get_news.py
+++ api/get_news.py
@@ -31,11 +31,11 @@ def scrape_news():
         news_items = soup.find_all('div', class_='part_list_2')
         if news_items:
             messages = []  # 創建一個列表來存儲新聞信息
-            for i, item in enumerate(news_items[0].find_all('h3')[:9]):  # 限制顯示前10條新聞
+            for i, item in enumerate(news_items[0].find_all('h3')[:3]):  # 限制顯示前3條新聞
                 title = item.text.strip()
                 relative_link = item.find('a')['href']
                 full_link = urljoin(url_fornews, relative_link)  # 將相對鏈接轉換為完整鏈接
-                message = f"隨選新聞 {i + 1}: {title}\n網址: {full_link}"
+                message = f"隨選新聞 {i + 1}: {title}\n網址: {full_link}\n"
                 messages.append(message)  # 將每條新聞信息添加到列表中
                 #print(message)
 
